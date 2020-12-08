bce0b6e65b8d834e1f5dba7f2d7e055cbb356295
ValarMorghulisSUSU <lenovoa736@gmail.com>
2020-12-08 16:21:50 +0500

    update

--- Controllers/ViewController.swift
+++ Controllers/ViewController.swift
@@ -7,27 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController{
 
     @IBOutlet weak var myLabel: UILabel!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//            guard segue.identifier == "showSecond" else { return }
-//            guard let destination = segue.destination as? NewWindow else { return }
-//            destination.name = "Андрей"
-//        }
-    
     @IBAction func openNewWindow(_ sender: Any) {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let vc = storyboard.instantiateViewController(withIdentifier: "NewWindow")
+        let vc = storyboard.instantiateViewController(withIdentifier: "NewWindow") as! newController
+        vc.newText = myLabel.text! + ", again!"
         self.present(vc, animated: true, completion: nil)
     }
     
-}
+}   
 
+
+
+class newController:UIViewController{ //newViewControllerDelegate{
+    var newText:String = ""
+    @IBOutlet weak var updatableLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateText(text: newText)
+        // Do any additional setup after loading the view.
+    }
+    func updateText(text:String) {
+        updatableLabel.text = text
+    }
+
+}
 
